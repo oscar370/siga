a8fd4ac2a6f1976fb451b3ff66024fc3870c291b
oscar370 <oscar.dga@outlook.com>
2026-05-05 16:24:25 +0000

    The role that can modify the data has been fixed

--- siga-frontend/hooks/use-can-mutate.ts
+++ siga-frontend/hooks/use-can-mutate.ts
@@ -3,5 +3,5 @@ import { useUserStore } from "@/stores/user-store";
 export function useCanMutate() {
   const roles = useUserStore((state) => state.roles);
 
-  return roles.includes("admin");
+  return roles.includes("Admin");
 }
